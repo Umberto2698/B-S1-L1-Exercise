0d5e9eee909c36219e2b7e127eebfc5c3c49289a
Umberto Zappulla <136740583+Umberto2698@users.noreply.github.com>
2023-10-02 15:54:11 +0200

    exercise three is now truly complete

--- Numero3.java
+++ Numero3.java
@@ -1,8 +1,30 @@
+import java.util.Scanner;
+
 public class Numero3 {
     public static void main(String[] args) {
-        System.out.println(perimetroRettangolo(10, 12.5));
-        System.out.println(pariDispari(22));
-        System.out.println(areaTrinagolo(12.5, 8, 5));
+        Scanner input = new Scanner(System.in);
+        System.out.println("Calcoliamo il perimetro di un rettangolo:");
+        System.out.println("Inserisci la lunghezza del primo lato");
+        double n1 = Double.parseDouble(input.nextLine());
+        System.out.println("Inserisci la lunghezza del secondo lato");
+        double n2 = Double.parseDouble(input.nextLine());
+        System.out.println("Il perimetro del rettangolo è " + perimetroRettangolo(n1, n2));
+        System.out.println("Inserisci un numero intero per scoprire se è pari o dispari:");
+        int n = Integer.parseInt(input.nextLine());
+        if (pariDispari(n) == 0) {
+            System.out.println(n + " è pari");
+        } else {
+            System.out.println(n + " è dispari");
+        }
+        System.out.println("Adesso calcoliamo l'area del rettangolo usando la formula di Erone:");
+        System.out.println("Inserisci la lunghezza del primo lato");
+        double l1 = Double.parseDouble(input.nextLine());
+        System.out.println("Inserisci la lunghezza del secondo lato");
+        double l2 = Double.parseDouble(input.nextLine());
+        System.out.println("Inserisci la lunghezza del terzo lato");
+        double l3 = Double.parseDouble(input.nextLine());
+        System.out.println("L'area del triangolo è " + areaTrinagolo(l1, l2, l3));
+        input.close();
 
     }
 
