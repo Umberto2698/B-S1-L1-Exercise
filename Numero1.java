b38550e8aa2835244b0c7ac9a71d5b6e568382f5
Umberto Zappulla <136740583+Umberto2698@users.noreply.github.com>
2023-10-02 15:12:18 +0200

    Completed first exercise

---
+++ Numero1.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class Numero1 {
+    public static void main(String[] args) {
+        System.out.println(Moltiplica(12, 3));
+        System.out.printf(Concatena("ciao ", 2));
+        String[] Arr = {"Ciao", "il", "mio", "nome", "è", ""};
+        System.out.println(Arrays.toString(InserisciInArray(Arr, "Umberto")));
+    }
+
+    public static int Moltiplica(int n1, int n2) {
+        return n1 * n2;
+    }
+
+    public static String Concatena(String str, int n) {
+        return str + " " + n;
+//        if (str.charAt(str.length()) != "b") {}
+    }
+
+    public static String[] InserisciInArray(String[] arr, String str) {
+        arr[5] = arr[4];
+        arr[4] = arr[3];
+        arr[3] = arr[2];
+        arr[2] = str;
+        return arr;
+    }
+}
